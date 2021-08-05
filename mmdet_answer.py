f7ed421b23448d91659cabea62cc5884d7641a4c
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2021-08-05 18:42:20 +0530

    remove unnecessary file

--- mmdet_answer.py
+++
@@ -1,8 +0,0 @@
-from mmdet.apis import init_detector, inference_detector
-import numpy as np
-mmdet_model = init_detector('./full_config.py', './epoch_12.pth', device='cuda:0')
-image_path = '1.png'
-result = inference_detector(mmdet_model, image_path)
-thresholdedResult = np.array(list(filter(lambda x : x[-1] > 0 , result[0][0])))  
-final_result = non_max_suppression_fast(thresholdedResult, .4) 
-print(result[0][0].tolist() )
